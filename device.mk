5178870f03a3a36bc64adbb8526f0ad2b64886f5
Skyline10124 <75871983+Skyline10124@users.noreply.github.com>
2024-02-12 10:38:00 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.kona
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.kona \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
